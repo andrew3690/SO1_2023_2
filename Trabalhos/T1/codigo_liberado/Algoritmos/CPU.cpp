e3f300467ad056cb36f02c463645393072ca718a
andrew3690 <videopineaplle16@gmail.com>
2023-08-30 22:35:27 -0300

    Adicionando a troca de contexto entre processos, falta inserir a cpu nesta troca (ponto de discussao)

--- Trabalhos/T1/codigo_liberado/Algoritmos/CPU.cpp
+++ Trabalhos/T1/codigo_liberado/Algoritmos/CPU.cpp
@@ -3,42 +3,42 @@
 
 CPU::Context::Context(){
     Context *pointer = new Context(); // Criacao de novo contexto
-    pointerList.push_back(pointer);   // Inserindo no fim da lista de processos
+    pointerList.push_back(pointer);   // Inserindo no fim da lista de processos prontos
+    context_counter++; // incrementa o contador de contextos, na criacao de um dado contexto
 }
+
 
 void CPU::Context::save(){
     // salva o contexto atual na fila de contextos
-    pointerList.push_back(this);
+    return pointerList.push_back(this);
 };
 
-void CPU::Context::load(Context *cont){
-    // Encontre o contexto a ser removido
-    auto it = pointerList.begin();
-    
-    for (; it != pointerList.end(); ++it) {
-        if (*it == cont) {
-            break; // Encontrou o contexto, sai do loop
-        }
-    }
-
-    if (it != pointerList.end()) {
-        // Remova o contexto da lista
-        pointerList.erase(it);
-    }
+void CPU::Context::load(){
+    // retorna o contexto a ser retirado da fila de prontos
+    return pointerList.remove(this);
 };
 
 CPU::Context::~Context()
 {
+    context_counter--; // decrementa o contador na destruicao de uma contexto
     delete pointer; // Libera a memoria alocada pelo processo
 };
 
 int CPU::switch_context(Context *from, Context *to)
 {
-    // trocar o contexto, primeiro salvando o contexto anterior
-    // depois trocando para o proximo contexto
-    
     // ideia:
     // salvar o contexto `from` na lista de contextos
+    // trocando os valores dos registradores, como fazer isso de forma que os atributos sejam privados ?
     // colocar o contexto `to` para rodar
-    // trocando os valores dos registradores, como fazer isso ?
+
+    from->save(); // Salva o contexto da cpu na lista de contextos
+
+    // realiza a troca dos valores de registradores e pilha de contexto
+    std::swap(from->SP,to->SP);
+    std::swap(from->PC,to->PC);
+    std::swap(from->ST,to->ST);
+    std::swap(from->registers,to->registers);
+    std::swap(from->_stack,to->_stack);
+
+    to->load(); // Carrega o contexto a ser trocado para execucao
 };
--- Trabalhos/T1/codigo_liberado/Algoritmos/definers/CPU.h
+++ Trabalhos/T1/codigo_liberado/Algoritmos/definers/CPU.h
@@ -15,22 +15,31 @@ class CPU{
                 ~Context(); // Destrutor do contexto
 
                 void save(); // metodo para salvar o contexto
-                void load(Context *cont); // metodo para carregar o contexto
+                void load(); // metodo para carregar o contexto
             
-            private:
+            public:
                 char *_stack; // Process Stack (manter aqui ou nao ?)
                 uint64_t SP; // STACK POINTER
                 uint64_t PC; // PROGRAM COUNTER
                 uint64_t ST; // STATUS REGISTER
                 uint64_t registers[6]; // R1,R2,R3,R4,R5,R6
-            public:
-                Context *pointer;      // pointeiro para o contexto atual
-                Context *save_pointer; // ponteiro para salvar o contexto
+
+                Context *pointer;    // pointeiro para o contexto atual
+                int context_counter; // contador de processos
                 std::list<Context*> pointerList; // Fila de contexto
         };
+
+        Context *cpu; // Contexto da CPU, para realizacao da troca de contexto
+        std::list<Context*> stack_context; // pilha de contextos
+        int switch_context(Context *from, Context *to);
     
-    Context *cpu; // Contexto da CPU, para realizacao da troca de contexto
-    static int switch_context(Context *from, Context *to);
+    private:
+        char *_stack; // CPU Process Stack (manter aqui ou nao ?)
+        uint64_t SP; // STACK POINTER
+        uint64_t PC; // PROGRAM COUNTER
+        uint64_t ST; // STATUS REGISTER
+        uint64_t registers[6]; // R1,R2,R3,R4,R5,R6
+
 };
 
 #endif // CPU_H
