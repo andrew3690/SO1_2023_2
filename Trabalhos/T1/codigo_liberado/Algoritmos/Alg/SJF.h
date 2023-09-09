a1046d2648a68eae793775c361b3188b7c6bd51e
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-09-09 18:58:01 -0300

    algoritmo preliminar sjf

--- Trabalhos/T1/codigo_liberado/Algoritmos/Alg/SJF.h
+++ Trabalhos/T1/codigo_liberado/Algoritmos/Alg/SJF.h
@@ -2,7 +2,7 @@
 #define SJF_SCHEDULER_H
 
 #include <vector>
-#include <definers/Process.h>
+#include "../definers/Process.h"
 
 using namespace std;
 
@@ -11,13 +11,15 @@ class ShortestJobFirst_Scheduler
 {
     public:
         ShortestJobFirst_Scheduler();
+        ~ShortestJobFirst_Scheduler();
 
 
-        void add_process(Process process_ready);
-        Process pop_first();
+        void add_process(int process_ready);
+        int schedule();
+        void sort();
 
     private:
-        vector<Process> process_list; // lista de processos
+        vector<int> process_list; // lista de processos
 
 };
 
---
+++ Trabalhos/T1/codigo_liberado/Algoritmos/SJF.cpp
@@ -0,0 +1,32 @@
+#include "Alg/SJF.h"
+
+
+ShortestJobFirst_Scheduler::ShortestJobFirst_Scheduler(){} //constructor
+
+ShortestJobFirst_Scheduler::~ShortestJobFirst_Scheduler(){} //destructor
+
+void ShortestJobFirst_Scheduler::add_process(int process1)
+{
+    process_list.push_back(process1); 
+    
+}
+
+int ShortestJobFirst_Scheduler::schedule()
+{
+    return *process_list.begin(); // retorna o primeiro elemento do array, com tempo de ex menor
+}
+
+void ShortestJobFirst_Scheduler::sort() 
+{
+    int size = process_list.size();
+    for (int i = 1; i < size; i++) {
+        int key = process_list[i];
+        int j = i-1;
+
+        while (key < process_list[j] && j >= 0) {
+            process_list[j + 1] = process_list[j];
+            j--;
+        }
+        process_list[j+1] = key;
+    }
+}
