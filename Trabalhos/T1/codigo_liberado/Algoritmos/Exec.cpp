9381d34a519b54e6c679c7d154a374b5b61c956a
andrew3690 <videopineaplle16@gmailc.om>
2023-09-09 18:43:48 -0300

    Ajustando makefile

--- Trabalhos/T1/codigo_liberado/Algoritmos/Exec.cpp
+++ Trabalhos/T1/codigo_liberado/Algoritmos/Exec.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
+// #include <iostream>
 #include "definers/Process.h"
 #include "definers/CPU.h"
 #include "read_file.cc"
-#include "Alg/FCFS.h"
+#include "FCFS.h"
 
+int main (){
     // idéia:
     //  criar um loop que aplique a cada entrada diferente um algoritmo de escalonamento do processador
     //  FCFS (FIRST Come First Served): 1
@@ -13,37 +14,50 @@
     //  Round Robin com quantum, de 2s sem prioridade: 5
     //  Finalizar: 6
 
-
-
-int main() {
-
     int input; // entrada que define qual escalonador utilizar
-
     while(true){
+        // Carregar os processo na fila de bloqueados da CPU
+
         std::cout << " Digite qual algoritmo de escalonamento deseja usar \n";
         std::cout << "1-FCFS, 2- SJF, 3- Por prioridade sem preempção, 4- Por prioridade com preempção, 5- Round Robin com quantum, de 2s sem prioridade, 6-Finalizar \n";
         std::cin >> input;
 
-        if(input = 6){
+        // Criando Objeto de Arquivo, e processando os parametros do arquivo
+        File f;
+        f.read_file(); 
+        std::vector<std::tuple<int, int, int>> params = f.get_process_params();
+        int count = 0;
+
+        // 
+        if(input == 6){
             break;
         }
-        else{
+        else {
             switch(input){
                 case 1:
-                    // FCFS
-
+                        // FCFS
+                        // realiza uma iteração do loop para cada processo na lista de processo
+                        for (const auto& tuple : params) {
+                            int creation_time = std::get<0>(tuple);
+                            int duration = std::get<1>(tuple);
+                            int priority = std::get<2>(tuple);
+                            count++;
+                            FCFS_Scheduler(count,creation_time,duration,priority);
+                        };
+                        break;
                 case 2:
                     // SJF
-
+                        break;
                 case 3:
                     // Prioridade sem preempção
-
+                        break;
                 case 4:
                     // Prioridade com preempção
-
+                        break;
                 case 5:
                     // Round Robin com quantum, de 2s sem prioridade
-            }
-        }
+                        break;
+            };
+        };
     };
-}
+};
--- Trabalhos/T1/codigo_liberado/Algoritmos/definers/Process.h
+++ Trabalhos/T1/codigo_liberado/Algoritmos/definers/Process.h
@@ -1,24 +1,51 @@
 #ifndef PROCESS_H
 #define PROCESS_H
+
 #include "CPU.h"
+#include <iostream>
+using namespace std;
 
 class Process: public CPU{
     public:
-        Process(int data, int time, int priority);
+        Process(int id, int data, int time, int priority);
         ~Process(); 
         
         void stop();  // interrrupcao do processo
-        void start(); // incia ou volta a execucao do processo 
+        void start(); // incia ou volta a execucao do processo
+        int makeready(int id); // faz o processo sair da fila de bloqueados para a fila de prontos, DEPENDE DO ESCANLONADOR UTILIZADO
+        void exec(); // método para execução do processo
+        void escalonate(); // método que os algoritmos de escalonamento chamam para escalonar processos
 
-    int id; // identificador
+        // getters de tempo e prioridade
+        int gettime(){return data_init;};
+        int getpriority(){return priority;};
+        int getid() {return _id;};
+        Process* FindProcessById(const std::list<Process*>& processList, int id);
+
+        
+        // Filas de processo prontos e pocessos bloqueados    
+        static std::list<Process*> Ready_queue;   // Fila de processos prontos
+        static std::list<Process*> Blocked_queue; // Fila de processo bloqueados
+        // const std::list<Process*>& processList;
 
     private:
+
         CPU::Context *ctxtpointer; // ponteiro de contexto do processo
+        Process *pointer; // ponteiro para o processo
+        Process *running; // ponteiro para o processo que está com a cpu no momento
+
+        static int id_increment; // incrementador do processo 
+        int _id; // identificador do processo
+
+        // Time diference
         int data_init; // data de inicio
         int data_end; // data de fim
-        int duration; // duracao
-        int timeexec; // tempo de execucao
-        int priority; // prioridade do processo 
+        int duration; // duracao, variável que faz uma estimativa do tempo de exeução do processo 
+        int timeexec; // tempo de execucao, variável que acompanha a exeucção do processo
+        
+        // Priority var
+        int priority; // prioridade do processo
+
         enum State // Estado atual
         {
             Ready,
@@ -26,6 +53,10 @@ class Process: public CPU{
             Running
         } state;
 
+        // Variáveis do trabalho
+        int tunarround_time = 0;
+        int read_list_avg_time = 0;
+        int switch_context_count = 0;
 };
 
 
