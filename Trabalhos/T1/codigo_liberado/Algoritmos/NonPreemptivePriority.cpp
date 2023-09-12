6d91202a6d43015e4b0a8fadc664b1ca53627b7f
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-09-11 23:20:20 -0300

    classe algoritmo prioridade

---
+++ Trabalhos/T1/codigo_liberado/Algoritmos/NonPreemptivePriority.cpp
@@ -0,0 +1,23 @@
+#include "NonPreemptivePriority.h"
+
+
+NonPPriorityScheduler::NonPPriorityScheduler() {}; //construtor
+NonPPriorityScheduler::~NonPPriorityScheduler() {}; //destrutor
+
+void NonPPriorityScheduler::schedule() {};
+
+void NonPPriorityScheduler::sort()
+{
+    // ordenar em ordem decrescente de prioridade (prioridade maior -> inicio do vetor)
+    int size = process_list.size();
+    for (int i = 1; i < size; i++) {
+        Process key = process_list[i];
+        int j = i-1;
+
+        while (key.getpriority() > process_list[j].getpriority() && j >= 0) {
+            process_list[j + 1] = process_list[j];
+            j--;
+        }
+        process_list[j+1] = key;
+    }
+}
