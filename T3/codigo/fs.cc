b46259f30ae0df70b7e4bdc54fc92bd9becd236a
andrew3690 <videopineaplle16@gmail.com>
2023-11-13 22:53:39 -0300

    Adicionando dados de debug, precisamos tratar os casos de blocos indiretos

--- T3/codigo/fs.cc
+++ T3/codigo/fs.cc
@@ -7,21 +7,47 @@ int INE5412_FS::fs_format()
 
 void INE5412_FS::fs_debug()
 {
-	union fs_block block;
+    union fs_block block;
 
-	disk->read(0, block.data);
+    disk->read(0, block.data);
 
-	cout << "superblock:\n";
-	cout << "    " << (block.super.magic == FS_MAGIC ? "magic number is valid\n" : "magic number is invalid!\n");
- 	cout << "    " << block.super.nblocks << " blocks\n";
-	cout << "    " << block.super.ninodeblocks << " inode blocks\n";
-	cout << "    " << block.super.ninodes << " inodes\n";
+    cout << "superblock:\n";
+    cout << "    " << (block.super.magic == FS_MAGIC ? "magic number is valid\n" : "magic number is invalid!\n");
+    cout << "    " << block.super.nblocks << " blocks\n";
+    cout << "    " << block.super.ninodeblocks << " inode blocks\n";
+    cout << "    " << block.super.ninodes << " inodes\n";
 
-	for(int i = 0; i < block.inode->size;i++){
-		cout <<"inode " << i <<":\n";
-		cout <<"size: " << block.data <<"bytes\n";
-		cout <<"direct blocks:"<< block.pointers <<"\n";
-	}
+    for (int i = 0; i < block.super.ninodes; ++i) {
+        fs_inode current_inode = block.inode[i];
+
+        // Verificar se o inode está em uso
+        if (current_inode.isvalid) {
+            cout << "inode " << i << ":\n";
+            cout << "    size: " << current_inode.size << " bytes\n";
+            cout << "    direct blocks: ";
+            for (int j = 0; j < POINTERS_PER_INODE; ++j) {
+                cout << current_inode.direct[j] << " ";
+            }
+            cout << "\n";
+
+            // Verificar se há um bloco indireto
+            if (current_inode.indirect) {
+                cout << "    indirect block: " << current_inode.indirect << "\n";
+                
+                // Lógica para ler o bloco indireto
+                union fs_block indirect_block;
+				
+                disk->read(current_inode.indirect, indirect_block.data);
+
+                // Exibir os ponteiros para os blocos de dados
+                cout << "    indirect block pointers: ";
+                for (int k = 0; k < disk->size(); ++k) {
+                    cout << indirect_block.pointers[k] << " ";
+                }
+                cout << "\n";
+            }
+        }
+    }
 }
 
 int INE5412_FS::fs_mount()
