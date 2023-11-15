4160fa3dd6a232fdaa3c62f1f3d260a78e6a268f
Ricardo Fachini <ricardofachinisouza@gmail.com>
2023-11-14 22:11:35 -0300

    debug percorre todos os blocos de inodes e imagem 20 original

--- T3/codigo/fs.cc
+++ T3/codigo/fs.cc
@@ -10,36 +10,41 @@ void INE5412_FS::fs_debug()
     union fs_block block;
     disk->read(0, block.data);
 
+    int nblocks = block.super.nblocks;  //numero de blocos ditado pelo superbloco
+
     cout << "superblock:\n";
     cout << "    " << (block.super.magic == FS_MAGIC ? "magic number is valid\n" : "magic number is invalid!\n");
     cout << "    " << block.super.nblocks << " blocks\n";
     cout << "    " << block.super.ninodeblocks << " inode blocks\n";
     cout << "    " << block.super.ninodes << " inodes\n";
 
-    // Iterar pelos inodes
-    for (int i = 0; i < block.super.ninodes; ++i) {
-        disk->read(i / INODES_PER_BLOCK + 1, block.data); // Carrega o bloco de inodes
+    //itera os blocos de inodes
+    for (int inode_block = 1; inode_block < block.super.ninodeblocks+1; inode_block++){
+        disk->read(inode_block, block.data); // Carrega o bloco de inodes
+        // Iterar pelos inodes
+        for (int i = 0; i < INODES_PER_BLOCK; ++i) {
 
-        // Calcula o índice dentro do bloco de inodes
-        int inode_index = i % INODES_PER_BLOCK;
+            // Calcula o índice dentro do bloco de inodes
+            int inode_index = i % INODES_PER_BLOCK;
 
-        // Verifica se o índice do inode é válido
-        if (block.inode[inode_index].isvalid) {
-            cout << "inode " << i << ":\n";
-            cout << "    size: " << block.inode[inode_index].size << " bytes\n";
-            cout << "    direct blocks:\n";
+            // Verifica se o índice do inode é válido
+            if (block.inode[inode_index].isvalid) {
+                cout << "inode " << i << ":\n";
+                cout << "    size: " << block.inode[inode_index].size << " bytes\n";
+                cout << "    direct blocks:\n";
 
-            // Iterar pelos ponteiros diretos do inode
-            for (int j = 0; j < POINTERS_PER_INODE; ++j) {
-                // Verificar se o ponteiro direto aponta para um bloco válido
-                if (block.inode[inode_index].direct[j] >= 0 && block.inode[inode_index].direct[j] < block.super.nblocks) {
-                    cout << "        " << block.inode[inode_index].direct[j] << "\n";
+                // Iterar pelos ponteiros diretos do inode
+                for (int j = 0; j < POINTERS_PER_INODE; ++j) {
+                    // Verificar se o ponteiro direto aponta para um bloco válido
+                    if (block.inode[inode_index].direct[j] >= 0 && block.inode[inode_index].direct[j] < nblocks) {
+                        cout << "        " << block.inode[inode_index].direct[j] << "\n";
+                    }
                 }
-            }
 
-            // Verificar se o ponteiro indireto aponta para um bloco válido
-            if (block.inode[inode_index].indirect >= 0 && block.inode[inode_index].indirect < block.super.nblocks) {
-                cout << "    indirect block: " << block.inode[inode_index].indirect << "\n";
+                // Verificar se o ponteiro indireto aponta para um bloco válido
+                if (block.inode[inode_index].indirect >= 0 && block.inode[inode_index].indirect < nblocks) {
+                    cout << "    indirect block: " << block.inode[inode_index].indirect << "\n";
+                }
             }
         }
     }
